d9bf703184319fc74c1790d2451869c90843e860
maclaun <onemoveoneshot@gmail.com>
2026-07-14 18:30:51 +0300

    fix: set N8N_BLOCK_ENV_ACCESS_IN_NODE=false in start script to allow node env access

--- start-n8n.bat
+++ start-n8n.bat
@@ -12,6 +12,9 @@ set "TELEGRAM_BOT_TOKEN=YOUR_TELEGRAM_BOT_TOKEN"
 set "TELEGRAM_CHAT_ID=YOUR_TELEGRAM_CHAT_ID"
 :: =====================================================
 
+:: Allow n8n nodes to access environment variables ($env.VAR)
+set N8N_BLOCK_ENV_ACCESS_IN_NODE=false
+
 :: Add Node.js and global npm to PATH
 set "PATH=C:\Program Files\nodejs;%APPDATA%\npm;%PATH%"
 
